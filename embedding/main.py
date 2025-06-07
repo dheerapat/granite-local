763678b164717014b1c807f49f428c5a853c378d
Dheerapat Tookkane <dheerapat38@gmail.com>
2025-06-07 17:11:47 +0700

    nit: run formatter

--- embedding/main.py
+++ embedding/main.py
@@ -1,9 +1,10 @@
 from openai import OpenAI
+
 client = OpenAI(base_url="http://192.168.110.142:1234/v1", api_key="local")
 
 response = client.embeddings.create(
     input="Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world.",
-    model="text-embedding-granite-embedding-278m-multilingual"
+    model="text-embedding-granite-embedding-278m-multilingual",
 )
 
 print(response.data[0].embedding)
